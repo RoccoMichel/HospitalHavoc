d65dbb3685456ffae5748133d4e672bd153cab1b
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2025-11-03 12:30:23 +0100

    Lunch Break

---
+++ Assets/Scripts/Patient.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Patient : MonoBehaviour
+{
+    internal float value = 0;
+
+    private void Start()
+    {
+        value = 5; // CHANGE THIS DEPENDING ON THE SICKNESS
+    }
+}
