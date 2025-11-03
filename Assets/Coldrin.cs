614c7f93564fcb7596981dfd40731ed8c51d2b47
Bolt_Bug <Charlie.stalhammar@gmail.com>
2025-11-03 12:35:14 +0100

    coldrin works, grounds for interaction script done

--- Assets/Coldrin.cs
+++ Assets/Coldrin.cs
@@ -1,28 +1,36 @@
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Coldrin : MonoBehaviour {
-    public List<ItemInfo> allPosibolItems; // fore ez´er item adishen
+public class Coldrin : MonoBehaviour
+{
+    public static Coldrin inctanse;
 
-    public static List<ItemInfo> AllPosibolItems; // list of all posibol items
-    public static List<ItemInfo> Ingerdens = new List<ItemInfo>(); // all the items curenty in the coldrin
+    public List<ItemInfo> allPosibolItems; // fore ezï¿½er item adishen
+
+    public List<ItemInfo> AllPosibolItems; // list of all posibol items
+    public List<ItemInfo> Ingerdens = new List<ItemInfo>(); // all the items curenty in the coldrin
 
     void Start() {
         AllPosibolItems = allPosibolItems;
     }
     // adds an igedent to the coldrin
-    public static void AddIngedent(ItemInfo info) {
+    public void AddIngedent(ItemInfo info) {
         Ingerdens.Add(info);
     }
 
     // mixes all ingedenst that ar in the coldrin
-    public static GameObject MixIngedents() {
+    public GameObject MixIngedents() {
         for (int i = 0; i < AllPosibolItems.Count; i++) {
             if (AllPosibolItems[i].ingrediants == Ingerdens)
                 return Instantiate(AllPosibolItems[i].item, Vector3.zero, Quaternion.identity);
         }
 
         return AllPosibolItems[0].item; // If resepy dusent exist
+    }
+
+    void Awake()
+    {
+        inctanse = this;
     }
 }
