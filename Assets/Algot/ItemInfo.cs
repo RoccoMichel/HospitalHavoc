bb8b8e4e896c8066ba4aa4cf0163010b31d089c2
Algot2 <156318062+Algot2@users.noreply.github.com>
2025-11-03 11:23:55 +0100

    Base ingredients

--- Assets/Algot/ItemInfo.cs
+++ Assets/Algot/ItemInfo.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New item", menuName = "Crea´te new item")]
+[CreateAssetMenu(fileName = "New item", menuName = "Scriptable Objects/Create new item")]
 public class ItemInfo : ScriptableObject {
     [Header("name/tag")]
     public string itemTag;
