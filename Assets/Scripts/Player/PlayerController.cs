c2f729d145272629a4c4718c156cd8c55f1f4747
Bolt_Bug <Charlie.stalhammar@gmail.com>
2025-11-03 14:07:49 +0100

    player rotates correctly now

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,8 +7,9 @@ public class PlayerController : MonoBehaviour
     CharacterController cc;
 
     public float movementSpeed;
+    public Transform orentation;
     Vector2 moveDir;
-
+    float yRot = 0;
     [Tooltip("0 = up, 1 = right, 2 = down, 3 = left")]
     public List<GameObject> moveDirObjects;
 
@@ -27,38 +28,56 @@ public class PlayerController : MonoBehaviour
     {
         moveDir = obj.ReadValue<Vector2>();
 
-        float yRot = 0;
-
         // up, down, left or right
-        if (moveDir.x > 0 && Mathf.Abs(moveDir.y) < 0.25f)
+        if (moveDir.x > 0 && Mathf.Abs(moveDir.y) < 0.25f)  //right
+        {
+            yRot = 0;
             ActivateDirObj(1);
-        else if (moveDir.x < 0 && Mathf.Abs(moveDir.y) < 0.25f)
+            orentation.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else if (moveDir.x < 0 && Mathf.Abs(moveDir.y) < 0.25f) //left
+        {
+            yRot = 0;
             ActivateDirObj(3);
-        else if (Mathf.Abs(moveDir.x) < 0.25f && moveDir.y > 0)
+            orentation.rotation = Quaternion.Euler(0, 270, 0);
+        }
+        else if (Mathf.Abs(moveDir.x) < 0.25f && moveDir.y > 0) //up
+        {
+            yRot = 0;
             ActivateDirObj(0);
-        else if (Mathf.Abs(moveDir.x) < 0.25f && moveDir.y < 0)
+            orentation.rotation = Quaternion.identity;
+        }
+        else if (Mathf.Abs(moveDir.x) < 0.25f && moveDir.y < 0) //down
+        {
+            yRot = 0;
             ActivateDirObj(2);
+            orentation.rotation = Quaternion.Euler(0, 180, 0);
+        }
 
         //diaginol
-        else if (moveDir.x > 0 && moveDir.y > 0)
+        else if (moveDir.x > 0 && moveDir.y > 0)    //up right
         {
             yRot = 45;
             ActivateDirObj(moveDir.x > moveDir.y ? 1 : 0);
+            orentation.rotation = Quaternion.Euler(0, 45, 0);
         }
-        else if (moveDir.x > 0 && moveDir.y < 0)
+        else if (moveDir.x > 0 && moveDir.y < 0)    //down right
         {
             yRot = -45;
             ActivateDirObj(moveDir.x > -moveDir.y ? 1 : 2);
+            orentation.rotation = Quaternion.Euler(0, 135, 0);
         }
-        else if (moveDir.x < 0 && moveDir.y > 0)
+        else if (moveDir.x < 0 && moveDir.y > 0)    //up left
         {
             yRot = -45;
             ActivateDirObj(-moveDir.x > moveDir.y ? 3 : 0);
+            orentation.rotation = Quaternion.Euler(0, 315, 0);
         }
-        else if (moveDir.x < 0 && moveDir.y < 0)
+        else if (moveDir.x < 0 && moveDir.y < 0)    //down left
         {
             yRot = 45;
             ActivateDirObj(moveDir.x > -moveDir.y ? 3 : 2);
+            orentation.rotation = Quaternion.Euler(0, 225, 0);
         }
 
         transform.rotation = Quaternion.Euler(0, yRot, 0);
@@ -115,7 +134,7 @@ public class PlayerController : MonoBehaviour
 
         foreach (GameObject item in search)
         {
-            if (Vector3.Dot(transform.forward, (item.transform.position - transform.position).normalized) >= Mathf.Cos(0.5f * pickUpFOV * Mathf.Deg2Rad))
+            if (Vector3.Dot(orentation.forward, (item.transform.position - transform.position).normalized) >= Mathf.Cos(0.5f * pickUpFOV * Mathf.Deg2Rad))
             {
                 if (Vector3.Distance(transform.position, item.transform.position) < closestDist)
                 {
@@ -130,7 +149,7 @@ public class PlayerController : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Vector3 forward = transform.forward;
+        Vector3 forward = orentation.forward;
         Vector3 up = transform.up;
 
         float radius = Mathf.Tan(pickUpFOV * 0.5f * Mathf.Deg2Rad) * pickUpRange;
