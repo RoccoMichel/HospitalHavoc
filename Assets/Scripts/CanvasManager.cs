9f10f24fad675dd8699cf4e22a231e32e1a7b6ff
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2025-11-04 10:50:51 +0100

    Recipe Book

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -7,6 +7,7 @@ public class CanvasManager : MonoBehaviour
     [SerializeField] private TMP_Text moneyDisplay;
     [SerializeField] private TMP_Text timeDisplay;
     private GameController gameController;
+    static private GameObject recipeBook;
     internal GameObject pauseMenu;
 
     void Start()
@@ -24,6 +25,12 @@ public class CanvasManager : MonoBehaviour
         moneyDisplay.text = '$' + Mathf.Ceil(gameController.money).ToString();
     }
 
+    public static void InstantiateRecipeBook()
+    {
+        if (recipeBook != null) return;
+        recipeBook = Instantiate((GameObject)Resources.Load("UI/Recipe Book"), GameController.gameController.canvasManager.transform);
+    }
+
     public static string GetTimerText(float time)
     {
         if (time < 0) return "0:00";
@@ -32,5 +39,10 @@ public class CanvasManager : MonoBehaviour
         int seconds = Mathf.FloorToInt(time % 60);
 
         return $"{minutes} : {(seconds < 10 ? 0 : string.Empty)}{seconds}";
+    }
+
+    private void OnDestroy()
+    {
+        recipeBook = null;
     }
 }
