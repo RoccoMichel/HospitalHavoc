e17ed232eced250cc36d861798cb012aec7d3d5f
Algot2 <156318062+Algot2@users.noreply.github.com>
2025-11-04 18:06:34 +0100

    Beter Spone boxes

--- Assets/SponeItem.cs
+++ Assets/SponeItem.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class SponeItem : MonoBehaviour {
     public ItemInfo spone;
+    public Transform sideObj;
     // spons the coret item and returns it
     public GameObject sponeItem() {
         return Instantiate(spone.item, transform.position, transform.rotation);
+    }
+
+    void Start() {
+        GameObject item = Instantiate(spone.item.transform.GetChild(0).gameObject);
+        item.transform.parent = sideObj.transform;
+        item.transform.localPosition = Vector3.zero;
+        item.transform.localScale *= 0.5f;
+        item.transform.localRotation = Quaternion.Euler(0,0,0);
+
     }
 }
