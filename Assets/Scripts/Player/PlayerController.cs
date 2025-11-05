81c2388553a81c9fa592214252ac4d5ebc69d889
Bolt_Bug <Charlie.stalhammar@gmail.com>
2025-11-05 10:24:51 +0100

    fixed player error

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -27,12 +27,17 @@ public class PlayerController : MonoBehaviour
         if (!GameController.gameController.active) return; // Freeze the player when paused
         cc.Move(new Vector3(moveDir.x, 0, moveDir.y) * movementSpeed * Time.deltaTime);
 
-        GameObject clossestItem = GetClosestObject(GameController.gameController.items);
+        GameObject clossestItem = GetClosestObject(GameController.gameController.interactables);
+        if (GameController.gameController.items.Count > 0)
+            clossestItem = GetClosestObject(GameController.gameController.items);
+
         GameObject clossestInteractable = GetClosestObject(GameController.gameController.interactables);
-        GameObject clossest = Vector3.Distance(transform.position, clossestItem.transform.position) <
-                              Vector3.Distance(transform.position, clossestInteractable.transform.position)
-                                  ? clossestItem
-                                  : clossestInteractable;
+        GameObject clossest = null;
+        if(clossestItem && clossestInteractable)
+            clossest = Vector3.Distance(transform.position, clossestItem.transform.position) <
+                       Vector3.Distance(transform.position, clossestInteractable.transform.position)
+                           ? clossestItem
+                           : clossestInteractable;
     }
 
     public void SetMoveDir(InputAction.CallbackContext obj)
