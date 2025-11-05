9776cd3710a7ecbada84469c36b5b8445ad7ad0b
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2025-11-05 10:19:43 +0100

    Patient Needs Display

--- Assets/VisuleseModels.cs
+++ Assets/VisuleseModels.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class VisuleseModels : MonoBehaviour
 {
-    public List<GameObject> objectInColdrin;
+    public float scaleFactor = 0.5f;
+    public Vector3 offset = new (0, 3, -5);
+    public List<GameObject> objectInColdrin = new();
     public void addItemToColdrin(ItemInfo item) {
         objectInColdrin.Add(Instantiate(item.item.transform.GetChild(0).gameObject));
-        objectInColdrin.Last().transform.localScale *= 0.5f;
+        objectInColdrin.Last().transform.localScale *= scaleFactor;
     }
     public void clerAll() {
         for (int i = 0; i < objectInColdrin.Count; i++) {
@@ -16,9 +18,9 @@ public class VisuleseModels : MonoBehaviour
         objectInColdrin = new();
     }
     void Update() {
-        Vector3 baseOfset = new Vector3(0, 3, -5) + transform.position;
+        Vector3 finalOffset = offset + transform.position;
         for (int i = 0; i < objectInColdrin.Count; i++) {
-            objectInColdrin[i].transform.position = Vector3.right * 0.5f * ((i+1) - objectInColdrin.Count/2f) + baseOfset;
+            objectInColdrin[i].transform.position = Vector3.right * 0.5f * ((i+1) - objectInColdrin.Count/2f) + finalOffset;
             objectInColdrin[i].transform.LookAt(Camera.main.transform);
         }
     }
