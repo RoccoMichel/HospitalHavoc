4bf3c42fa501b6f18d1c79039f0b0e3dbf5c591c
Algot2 <156318062+Algot2@users.noreply.github.com>
2025-11-06 19:31:50 +0100

    Bunsh of shit

---
+++ Assets/PasentVisholsRandomiser.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasentVisholsRandomiser : MonoBehaviour
+{
+    public List<Sprite> Skin, UperBody, Fase, Haer, Pants, Shose;
+    public List<SpriteRenderer> Renderers;
+
+    Sprite GetRan(List<Sprite> sprits) => 
+        sprits[Random.Range(0, sprits.Count)];
+    public void RandomiseAperns() {
+        Renderers[0].sprite = GetRan(Skin);
+        Renderers[1].sprite = GetRan(UperBody);
+        Renderers[2].sprite = GetRan(Fase);
+        Renderers[3].sprite = GetRan(Haer);
+        Renderers[4].sprite = GetRan(Pants);
+        Renderers[5].sprite = GetRan(Shose);
+    }
+    private void Start()
+    {
+        RandomiseAperns();
+    }
+}
